b68dcdef1b3d751e83ee86b237c6b34eebf45c4f
edunham <dunhame@onid.orst.edu>
2013-10-24 11:26:37 -0700

    rtfd_conf: attempt to work around rtd's dislike of Hieroglyph

---
+++ source/rtfd_conf.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# OSU DevOps Bootcamp documentation build configuration file, created by
+# sphinx-quickstart on Tue Oct 15 12:20:17 2013.
+#
+# This file is execfile()d with the current directory set to its containing dir.
+#
+
+from conf import *
+
+extensions = ['hieroglyph', 'sphinx.ext.todo', 'sphinx.ext.ifconfig']
+
