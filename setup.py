ed7bdff1f537f9db8dee7ab7e8aa98f9e7bd5dc2
Emily Dunham <edunham@osuosl.org>
2013-11-12 15:19:10 -0800

    setup: take a wild guess and see what happens

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="DevOps Bootcamp",
+      version='0.0.1',
+      description="OSU OSL's training curriculum",
+      author='OSU OSL & OSU LUG',
+      author_email='devopsbootcamp@osuosl.org',
+      url='http://devopsbootcamp.osuosl.org',
+      packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+      zip_safe=False,
+)
