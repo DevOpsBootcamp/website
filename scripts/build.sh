ed80f2f838370841d8c0c31df12e7c6d394fc03a
edunham <dunhame@onid.orst.edu>
2014-01-30 16:45:35 -0800

    Add build script
    Trying to set up jenkins on slides.o.o to facilitate automatic deployment

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Update repo
+git checkout master
+git pull -q --ff-only
+
+# Enable venv
+if [ ! -d venv ]; then
+    virtualenv venv
+fi
+source venv/bin/activate
+
+# Update packages
+pip install -r requirements.txt
+
+# Build docs
+make slides
+
+# Disable venv
+deactivate
+
