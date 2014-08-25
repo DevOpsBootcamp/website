6d8c71b8a02fbbb33132d5f6fc0214235f243a1e
Jordan Evans <jordane@osuosl.org>
2014-08-25 11:03:55 -0700

    add build_website.sh script

---
+++ scripts/build_website.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Update repo
+git checkout master
+git pull -q --ff-only
+
+# Enable venv
+if [ ! -d venv ]; then
+    virtualenv venv
+fi
+source venv/bin/activate
+
+# Update packages
+pip install -r requirements.txt
+
+# Build websute
+make html
+
+# Disable venv
+deactivate
